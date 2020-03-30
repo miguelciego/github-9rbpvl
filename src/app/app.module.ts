d67e8dc6520285cf9369030c24d0cbe685326d77
Miguel Aridany Sánchez López <miguelciego@gmail.com>
2020-03-29 22:03:43 -0400

    Actualizando version de Angular, add papaparse

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { Tabla1Component } from './tabla1/tabla1.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
