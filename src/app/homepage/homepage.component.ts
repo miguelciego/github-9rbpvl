e3a9c47d7e44e91cd01f35b8edacb3ff373239f1
miguelciego <miguelciego@hotmail.com>
2020-03-31 14:36:12 -0400

    Load fichero

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import  Papa  from 'papaparse';
+import { MatTableDataSource } from '@angular/material/table';
 
 
 @Component({
@@ -8,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
+  dataSource = new MatTableDataSource();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+onChange(files: File[]){
+    
+    if(files[0]){
+      //console.log(files[0]);
+      Papa.parse(files[0], {
+        header: true,
+        skipEmptyLines: true,
+        complete: (result,file) => {
+          //console.log(result);
+          this.dataSource.data=result.data;
+          console.log(this.dataSource.data);
+          //this.dataList = result.data;
+        }
+      });
+    }
   }
 
 }
